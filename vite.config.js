be1ef6ee6ca6dd3ce98e3136f5df105082e29d06
Nick <34172910+NickGeronimo@users.noreply.github.com>
2024-06-20 18:03:10 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
+    base:'/IphoneClone',
     org: "jsm-gya",
     project: "javascript-react"
   })],
